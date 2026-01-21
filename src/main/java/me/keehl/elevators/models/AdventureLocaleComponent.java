2f054176b1845ded0814bd9ccf3a3d0c128b93bc
Keehl <roblox.keehl254@gmail.com>
2026-01-20 19:46:43 -0600

    Placeholders support

--- src/main/java/me/keehl/elevators/models/AdventureLocaleComponent.java
+++ src/main/java/me/keehl/elevators/models/AdventureLocaleComponent.java
@@ -1,11 +1,14 @@
 package me.keehl.elevators.models;
 
+import me.keehl.elevators.Elevators;
 import me.keehl.elevators.api.models.IElevatorEventData;
 import me.keehl.elevators.api.models.ILocaleComponent;
-import me.keehl.elevators.helpers.MessageHelper;
+import me.keehl.elevators.helpers.ElevatorHelper;
 import net.kyori.adventure.audience.Audience;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.MiniMessage;
+import net.kyori.adventure.text.minimessage.tag.Tag;
+import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver;
 import net.kyori.adventure.text.serializer.bungeecord.BungeeComponentSerializer;
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import net.md_5.bungee.api.chat.BaseComponent;
@@ -17,8 +20,8 @@ public class AdventureLocaleComponent implements ILocaleComponent {
     private final String componentMessage;
 
     public AdventureLocaleComponent(String message) {
-        message = ChatColor.translateAlternateColorCodes('&',message);
-        if(message.contains(ChatColor.COLOR_CHAR + "")) {
+        message = ChatColor.translateAlternateColorCodes('&', message);
+        if (message.contains(ChatColor.COLOR_CHAR + "")) {
             message = MiniMessage.miniMessage().serialize(LegacyComponentSerializer.legacySection().deserialize(message));
         }
         this.componentMessage = message;
@@ -33,21 +36,46 @@ public class AdventureLocaleComponent implements ILocaleComponent {
     }
 
     private Component getFormattedComponent(IElevatorEventData eventData) {
-        String newComponent = MessageHelper.formatPlaceholders(eventData.getPlayer(), this.componentMessage);
+        String newComponent = Elevators.getHooksService().getPlaceholderHook().formatPlaceholders(eventData.getPlayer(), this.componentMessage);
 
-        return MiniMessage.miniMessage().deserialize(newComponent);
+        TagResolver.Builder resolver = TagResolver.builder();
+        if(eventData.getPlayer() != null) {
+            resolver.tag("player", Tag.preProcessParsed(eventData.getPlayer().getName()));
+        }
+        resolver.tag("elevators_type", Tag.preProcessParsed(eventData.getOrigin().getElevatorType(false).getTypeKey()));
+        resolver.tag("elevators_type_display", Tag.inserting(MiniMessage.miniMessage().deserialize(eventData.getOrigin().getElevatorType(false).getDisplayName().serialize())));
+
+        if(eventData.getDestination() != null) {
+            if(newComponent.contains("<elevators_new_floor>"))
+                resolver.tag("elevators_new_floor", Tag.preProcessParsed(ElevatorHelper.getFloorNumberOrCount(eventData.getDestination(), true)+""));
+            if(newComponent.contains("<elevators_top_floor>"))
+                resolver.tag("elevators_top_floor", Tag.preProcessParsed(ElevatorHelper.getFloorNumberOrCount(eventData.getDestination(), false)+""));
+            if(newComponent.contains("<elevators_new_floor_name>"))
+                resolver.tag("elevators_new_floor_name", Tag.preProcessParsed(Elevators.getDataContainerService().getFloorName(eventData.getDestination())));
+        }
+
+        if(eventData.getOrigin() != null) {
+            if(newComponent.contains("<elevators_old_floor>"))
+                resolver.tag("elevators_old_floor", Tag.preProcessParsed(ElevatorHelper.getFloorNumberOrCount(eventData.getOrigin(), true)+""));
+            if(newComponent.contains("<elevators_top_floor>") && eventData.getDestination() == null)
+                resolver.tag("elevators_top_floor", Tag.preProcessParsed(ElevatorHelper.getFloorNumberOrCount(eventData.getDestination(), false)+""));
+            if(newComponent.contains("<elevators_old_floor_name>"))
+                resolver.tag("elevators_old_floor_name", Tag.preProcessParsed(Elevators.getDataContainerService().getFloorName(eventData.getOrigin())));
+        }
+
+        return MiniMessage.miniMessage().deserialize(newComponent, resolver.build());
     }
 
     @Override
     public void send(CommandSender commandSender) {
-        if(!(commandSender instanceof Audience audience))
+        if (!(commandSender instanceof Audience audience))
             return;
         audience.sendMessage(this.getComponent());
     }
 
     @Override
     public void sendFormatted(CommandSender commandSender, IElevatorEventData eventData) {
-        if(!(commandSender instanceof Audience audience))
+        if (!(commandSender instanceof Audience audience))
             return;
         audience.sendMessage(this.getFormattedComponent(eventData));
     }
--- src/main/java/me/keehl/elevators/models/BungeeLocaleComponent.java
+++ src/main/java/me/keehl/elevators/models/BungeeLocaleComponent.java
@@ -1,7 +1,9 @@
 package me.keehl.elevators.models;
 
+import me.keehl.elevators.Elevators;
 import me.keehl.elevators.api.models.IElevatorEventData;
 import me.keehl.elevators.api.models.ILocaleComponent;
+import me.keehl.elevators.helpers.ElevatorHelper;
 import net.md_5.bungee.api.chat.BaseComponent;
 import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.ChatColor;
@@ -25,7 +27,47 @@ public class BungeeLocaleComponent implements ILocaleComponent {
     }
 
     private BaseComponent[] getFormattedComponent(IElevatorEventData eventData) {
-        return TextComponent.fromLegacyText(this.componentMessage);
+        String newComponent = Elevators.getHooksService().getPlaceholderHook().formatPlaceholders(eventData.getPlayer(), this.componentMessage);
+
+        if(eventData.getPlayer() != null) {
+            newComponent = newComponent.replace("%player%", eventData.getPlayer().getName()).replace("<player>", eventData.getPlayer().getName());
+        }
+        String elevatorTypeKey = eventData.getOrigin().getElevatorType(false).getTypeKey();
+        String elevatorTypeDisplay = eventData.getOrigin().getElevatorType(false).getDisplayName().toLegacyText();
+        newComponent = newComponent.replace("%elevators_type%", elevatorTypeKey).replace("<elevators_type>", elevatorTypeKey);
+        newComponent = newComponent.replace("%elevators_type_display%", elevatorTypeDisplay).replace("<elevators_type_display>", elevatorTypeDisplay);
+
+        if(eventData.getDestination() != null) {
+            if(newComponent.contains("<elevators_new_floor>") || newComponent.contains("%elevators_new_floor%")) {
+                String value = ElevatorHelper.getFloorNumberOrCount(eventData.getDestination(), true) + "";
+                newComponent = newComponent.replace("%elevators_new_floor%", elevatorTypeDisplay).replace("<elevators_new_floor>", value);
+            }
+            if(newComponent.contains("<elevators_top_floor>") || newComponent.contains("%elevators_top_floor%")) {
+                String value = ElevatorHelper.getFloorNumberOrCount(eventData.getDestination(), false) + "";
+                newComponent = newComponent.replace("%elevators_type_display%", elevatorTypeDisplay).replace("<elevators_type_display>", value);
+            }
+            if(newComponent.contains("<elevators_new_floor_name>") || newComponent.contains("%elevators_new_floor_name%")) {
+                String value = Elevators.getDataContainerService().getFloorName(eventData.getDestination());
+                newComponent = newComponent.replace("%elevators_new_floor_name%", elevatorTypeDisplay).replace("<elevators_new_floor_name>", value);
+            }
+        }
+
+        if(eventData.getOrigin() != null) {
+            if(newComponent.contains("<elevators_old_floor>") || newComponent.contains("%elevators_old_floor%")) {
+                String value = ElevatorHelper.getFloorNumberOrCount(eventData.getOrigin(), true) + "";
+                newComponent = newComponent.replace("%elevators_new_floor%", elevatorTypeDisplay).replace("<elevators_new_floor>", value);
+            }
+            if((newComponent.contains("<elevators_top_floor>") || newComponent.contains("%elevators_top_floor%") ) && eventData.getDestination() == null) {
+                String value = ElevatorHelper.getFloorNumberOrCount(eventData.getDestination(), false) + "";
+                newComponent = newComponent.replace("%elevators_type_display%", elevatorTypeDisplay).replace("<elevators_type_display>", value);
+            }
+            if(newComponent.contains("<elevators_old_floor_name>") || newComponent.contains("%elevators_old_floor_name%")) {
+                String value = Elevators.getDataContainerService().getFloorName(eventData.getOrigin());
+                newComponent = newComponent.replace("%elevators_old_floor_name%", elevatorTypeDisplay).replace("<elevators_old_floor_name>", value);
+            }
+        }
+
+        return TextComponent.fromLegacyText(newComponent);
     }
 
     @Override
